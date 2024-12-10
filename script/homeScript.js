e763e57407a9f1ebc7a738caa6dc1880f84c2abf
tmdalsdl <lsm123452002@gmail.com>
2024-12-10 11:01:24 +0900

    update

--- script/homeScript.js
+++ script/homeScript.js
@@ -1,25 +1,60 @@
-// homescript.js
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+// homeScript.js
+document.addEventListener('DOMContentLoaded', function() {
+    let isButtonClicked = false;
+
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
-});
 
-// Redirect to specific project page on portfolio item click
-document.querySelectorAll('.portfolio-content1').forEach((item, index) => {
-    item.addEventListener('click', function () {
-        // Assuming your project pages follow a naming convention like project1.html, project2.html, etc.
-        window.location.href = `CloudProject/project${index + 1}.html`;
+    document.querySelectorAll('.likes').forEach((img, index) => {
+        img.addEventListener('mousedown', function(event) {
+            event.stopPropagation();
+            img.src = 'image/likes2.png';
+            isButtonClicked = true;
+        });
+
+        img.addEventListener('mouseup', function(event) {
+            event.stopPropagation();
+            img.src = 'image/likes.png';
+            if (isButtonClicked) {
+                const buttonId = `likesProject${index + 1}`;
+                
+                // API 호출 예시
+                fetch(`https://wihfjpwi54.execute-api.ap-northeast-2.amazonaws.com/portfolio2024/projects/${buttonId}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ buttonId })
+                })
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error:', error));
+
+                isButtonClicked = false;
+            }
+        });
     });
-});
 
-document.querySelectorAll('.portfolio-content2').forEach((item, index) => {
-    item.addEventListener('click', function () {
-        // Assuming your project pages follow a naming convention like project1.html, project2.html, etc.
-        window.location.href = `WebProject/project${index + 1}.html`;
+    document.querySelectorAll('.portfolio-content1').forEach((item, index) => {
+        item.addEventListener('click', function(event) {
+            if (!isButtonClicked) {
+                window.location.href = `CloudProject/project${index + 1}.html`;
+            }
+        });
+    });
+
+    document.querySelectorAll('.portfolio-content2').forEach((item, index) => {
+        item.addEventListener('click', function(event) {
+            if (!isButtonClicked) {
+                window.location.href = `WebProject/project${index + 1}.html`;
+            }
+        });
     });
 });
